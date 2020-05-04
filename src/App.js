7c5af2309aa74b35fc703bad4621e0665a7a5758
LIBINKAI <841116931@qq.com>
2020-05-05 00:05:06 +0800

    add styled-components and reset.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { IndexStyle } from './index-style';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <IndexStyle />
+      Hello World !
     </div>
   );
 }
